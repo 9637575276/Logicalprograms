fdfe07fd0ccb54c7284f4f29ed22a2b7265887c3
Shubham Mule <shubhambmule@gmail.com>
2021-09-06 15:00:22 +0530

    series

--- Logical Programs/Program.cs
+++ Logical Programs/Program.cs
@@ -6,22 +6,20 @@ namespace Logical_Programs
     {
         static void Main(string[] args)
         {
-            int num, i, ctr = 0;
-
-            Console.Write("Input  a number: ");
-            num = Convert.ToInt32(Console.ReadLine());
-            for (i = 2; i <= num / 2; i++)
+            int n1 = 0, n2=1 , n3, i, number;
+            number = int.Parse(Console.ReadLine());
+           
+            Console.Write("Enter a number of numbers");
+            Console.Write(n1 + " " + n2 + " ");
+            
+            for (i=2;i<=number;i++)
             {
-                if (num % i == 0)
-                {
-                    ctr++;
-                    break;
-                }
+                n3 = n1 + n2;
+                Console.Write(n3+" ");
+                n1 = n2;
+                n2 = n3;
             }
-            if (ctr == 0 && num != 1)
-                Console.Write("{0} is a prime number.\n", num);
-            else
-                Console.Write("{0} is not a prime number\n", num);
+
         }
     }
 }
