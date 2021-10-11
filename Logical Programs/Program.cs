023a6c8a5f3206e782e56eaaa276675d30a9e3a9
Shubham Mule <79191589+9637575276@users.noreply.github.com>
2021-10-11 15:44:52 +0530

    Update Program.cs

--- Logical Programs/Program.cs
+++ Logical Programs/Program.cs
@@ -7,9 +7,9 @@ namespace Logical_Programs
         static void Main(string[] args)
         {
             int n1 = 0, n2=1 , n3,  number;
+            Console.Write("Enter a number");
             number = int.Parse(Console.ReadLine());
-           
-            Console.Write("Enter a number of numbers");
+        
             Console.Write(n1 + " " + n2 + " ");
             
             for (int i=2;i<=number;i++)
